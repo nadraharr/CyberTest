e3ea0c0f13b4c8ce8148a235d5b7d10da32f28a7
nadraharr <nadraharr@gmail.com>
2023-12-06 01:31:19 +0200

    Rubocop: before

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,57 +1,49 @@
 class PagesController < ApplicationController
+  def index; end
 
-  def index    
-  end
-
-  def getgit(next_page_option = nil, prev_page_option = nil, first_option = "first: 20", last_option = nil)    
-    
-    token = ENV["GITHUB_TOKEN"]
-    git_api_url = "https://api.github.com/graphql"    
-    user_login = params[:user_login] || params[:user_input].to_s.strip    
+  def getgit(next_page_option = nil, prev_page_option = nil, first_option = 'first: 20', last_option = nil)
+    token = ENV['GITHUB_TOKEN']
+    git_api_url = 'https://api.github.com/graphql'
+    user_login = params[:user_login] || params[:user_input].to_s.strip
     query = <<~GQL
-            query {
-            user(login: "#{user_login}") {
-            name
-            login
-            repositories(#{first_option}, #{last_option}, #{next_page_option}, #{prev_page_option}, ownerAffiliations: OWNER, isFork: false) {
-            nodes {
-              name
-              url
-            }
-            pageInfo {
-              hasPreviousPage
-              hasNextPage
-              startCursor
-              endCursor
-            }
-            }
-            }
-            }
-            GQL
+      query {
+      user(login: "#{user_login}") {
+      name
+      login
+      repositories(#{first_option}, #{last_option}, #{next_page_option}, #{prev_page_option}, ownerAffiliations: OWNER, isFork: false) {
+      nodes {
+        name
+        url
+      }
+      pageInfo {
+        hasPreviousPage
+        hasNextPage
+        startCursor
+        endCursor
+      }
+      }
+      }
+      }
+    GQL
 
-      connection = Faraday.new(url: git_api_url) do |faraday|
+    connection = Faraday.new(url: git_api_url) do |faraday|
       faraday.headers['Content-Type'] = 'application/json'
       faraday.headers['Authorization'] = "Bearer #{token}"
       faraday.adapter Faraday.default_adapter
-      end
+    end
 
-      response = connection.post do |req|
-        req.body = { query: query }.to_json
-      end  
+    response = connection.post do |req|
+      req.body = { query: }.to_json
+    end
 
-        @result = JSON.parse(response.body)
-      end
+    @result = JSON.parse(response.body)
+  end
 
+  def next_page
+    getgit("after: \"#{params[:cursor_value]}\"", nil, 'first: 20', nil)
+  end
 
-      
-      def next_page   
-        getgit("after: \"#{params[:cursor_value]}\"", nil, "first: 20", nil)     
-      end
-      
-      def prev_page
-        getgit(nil, "before: \"#{params[:cursor_value]}\"", nil, "last: 20")
-      end
-      
+  def prev_page
+    getgit(nil, "before: \"#{params[:cursor_value]}\"", nil, 'last: 20')
+  end
 end
-
-
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
   root 'pages#index'
-  post "/submit", to: 'pages#getgit'
-  post "/nextpage", to: 'pages#next_page'
-  post "/prevpage", to: 'pages#prev_page'
-  if Rails.env.development?
-    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
-  end
-  post "/graphql", to: "graphql#execute"
+  post '/submit', to: 'pages#getgit'
+  post '/nextpage', to: 'pages#next_page'
+  post '/prevpage', to: 'pages#prev_page'
+  mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql' if Rails.env.development?
+  post '/graphql', to: 'graphql#execute'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
